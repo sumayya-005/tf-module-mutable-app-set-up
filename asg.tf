d9e6d55b7063f6b74ad57cf607c8e370d6441ff6
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-04-11 17:10:39 +0530

    updating ./tf-module-mutable-app-set-up

--- asg.tf
+++ asg.tf
@@ -5,9 +5,11 @@ resource "aws_launch_template" "launch-template" {
 }
 
 resource "aws_autoscaling_group" "bar" {
+  name = "${var.env}-${var.name}-asg"
   desired_capacity   = var.min_size
   max_size           = var.max_size
   min_size           = var.min_size
+  vpc_zone_identifier = var.subnets
 
   launch_template {
     id      = aws_launch_template.launch-template.id
