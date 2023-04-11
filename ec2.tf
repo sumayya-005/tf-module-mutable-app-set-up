baf4ebf044941201859c603df43d8694f9ac5a33
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-04-11 17:02:41 +0530

    updating ./tf-module-mutable-app-set-up

--- ec2.tf
+++ ec2.tf
@@ -3,3 +3,14 @@ resource "aws_launch_template" "launch-template" {
   image_id      = data.aws_ami.centos-8-ami.image_id
   instance_type = var.instance_type
 }
+
+resource "aws_autoscaling_group" "bar" {
+  desired_capacity   = 1
+  max_size           = 1
+  min_size           = 1
+
+  launch_template {
+    id      = aws_launch_template.launch-template.id
+    version = "$Latest"
+  }
+}
