a3d4015907eb6ee4b245d29e8478b61c89e30468
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-04-12 20:03:10 +0530

    updating ./tf-module-mutable-app-set-up

--- asg.tf
+++ asg.tf
@@ -2,8 +2,12 @@ resource "aws_launch_template" "launch-template" {
   name          = "${var.env}-${var.name}-lt"
   image_id      = data.aws_ami.centos-8-ami.image_id
   instance_type = var.instance_type
-  user_data     = base64encode(templatefile("${path.module}/ansible-pull.sh")
-    }
+  user_data     = filebase64("${path.module}/ansible-pull.sh")
+  COMPONENT       = var.name
+  ENV           = var.env
+   }))
+
+}
 
 resource "aws_autoscaling_group" "bar" {
   name = "${var.env}-${var.name}-asg"
