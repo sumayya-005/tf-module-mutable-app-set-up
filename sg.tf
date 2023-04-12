b39693aaf24a45cb8ca71437823b43c89bba0625
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-04-12 22:14:03 +0530

    updating ./tf-module-mutable-app-set-up

--- sg.tf
+++ sg.tf
@@ -1,41 +1,26 @@
-#resource "aws_security_group" "sg" {
-#  name        = "${var.env}-${var.name}-ec2.sg"
-#  description = "${var.env}-${var.name}-ec2.sg"
-#  vpc_id      = var.vpc_id
-#
-#  ingress {
-#    description = "SSH"
-#    from_port   = 22
-#    to_port     = 22
-#    protocol    = "tcp"
-#    cidr_blocks = [var.BASTION_NODE]
-#  }
-#
-#  ingress {
-#    description = "APP"
-#    from_port   = var.app_port_no
-#    to_port     = var.app_port_no
-#    protocol    = "tcp"
-#    cidr_blocks = [var.vpc_cidr]
-#  }
-#
-#  ingress {
-#    description = "PROMETHEUS"
-#    from_port   = 9100
-#    to_port     = 9100
-#    protocol    = "tcp"
-#    cidr_blocks = [var.PROMETHEUS_NODE]
-#  }
-#
-#  egress {
-#    from_port        = 0
-#    to_port          = 0
-#    protocol         = "-1"
-#    cidr_blocks      = ["0.0.0.0/0"]
-#    ipv6_cidr_blocks = ["::/0"]
-#  }
-#
-#  tags = {
-#    Name = "${var.env}-${var.name}-ec2.sg"
-#  }
-#}
+resource "aws_security_group" "" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.main.cidr_block]
+    ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
