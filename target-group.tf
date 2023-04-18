a7e7d07f151412c7772904d9bc84555da90de2b3
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-04-18 15:39:47 +0530

    updating ./tf-module-mutable-app-set-up

--- target-group.tf
+++ target-group.tf
@@ -12,3 +12,26 @@ resource "aws_lb_target_group" "main" {
     path = "/health"
   }
 }
+
+resource "aws_lb_listener_rule" "rule" {
+  count        = var.type
+  listener_arn = var.alb ["private"].lb_listener_arn
+  priority     = 100
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.static.arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["/static/*"]
+    }
+  }
+
+  condition {
+    host_header {
+      values = ["example.com"]
+    }
+  }
+}
--- vars.tf
+++ vars.tf
@@ -6,6 +6,9 @@ variable "instance_type" {}
 variable "min_size" {}
  variable "vpc_id" {}
  variable "BASTION_NODE" {}
- variable "private_zone_id" {}
  variable "app_port_no" {}
+ variable "vpc_cidr" {}
+ variable "private_zone_id" {}
  variable"PROMETHEUS_NODE" {}
+  variable "lb_listener_priority"{}
+ variable "type" {}
