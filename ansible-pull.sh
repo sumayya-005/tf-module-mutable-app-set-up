0eec23d039ec8f12cb1f1efe19c59eaef190ba2f
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-04-20 11:43:40 +0530

    updating ./tf-module-mutable-app-set-up

--- ansible-pull.sh
+++ ansible-pull.sh
@@ -5,3 +5,6 @@ yum install python39-devel -y &>>$LOG
 pip3.9 install ansible botocore boto3 &>>$LOG
 
 ansible-pull -i localhost, -U https://github.com/sumayya-005/roboshop-ansible roboshop.yml -e ROLE_NAME=${COMPONENT} -e ENV=${ENV} &>>$LOG
+
+
+#This is going to give application content we need for the app setup.
