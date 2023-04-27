de3c8a0c9a03285492d0cfcd5894d17430fbf03f
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-04-27 15:11:00 +0530

    updating ./tf-module-mutable-app-set-up

--- asg.tf
+++ asg.tf
@@ -2,7 +2,7 @@ resource "aws_launch_template" "launch-template" {
   name                   = "${var.env}-${var.name}-lt"
   image_id               = data.aws_ami.centos-8-ami.image_id
   instance_type          = var.instance_type
-  vpc_security_group_ids = [aws_security_group.main.id]
+#  vpc_security_group_ids = [aws_security_group.main.id]
 
 #  iam_instance_profile {
 #    name = aws_iam_instance_profile.instance_profile.name
@@ -24,7 +24,7 @@ resource "aws_autoscaling_group" "asg" {
   desired_capacity   = var.min_size
   max_size           = var.max_size
   min_size           = var.min_size
-  vpc_zone_identifier = var.subnets
+#  vpc_zone_identifier = var.subnets
 #  target_group_arns = [aws_lb_target_group.main.arn]
 
   launch_template {
@@ -32,8 +32,8 @@ resource "aws_autoscaling_group" "asg" {
     version = "$Latest"
   }
   tag  {
-    key = "Name"
-    value = "${var.env}-${var.name}"
+    key         = "Name"
+    value       = "${var.env}-${var.name}"
     propagate_at_launch = true
   }
 }
