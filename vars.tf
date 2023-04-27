f9dc41e79000db7660a0cc09513c2bfe9c6b0617
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-04-27 15:08:53 +0530

    updating ./tf-module-mutable-app-set-up

--- vars.tf
+++ vars.tf
@@ -5,11 +5,11 @@ variable "instance_type" {}
  variable "max_size" {}
 variable "min_size" {}
  variable "vpc_id" {}
- variable "BASTION_NODE" {}
- variable "app_port_no" {}
- variable "vpc_cidr" {}
- variable "private_zone_id" {}
- variable"PROMETHEUS_NODE" {}
+# variable "BASTION_NODE" {}
+# variable "app_port_no" {}
+# variable "vpc_cidr" {}
+# variable "private_zone_id" {}
+# variable"PROMETHEUS_NODE" {}
 #  variable "lb_listener_priority"{}
 # variable "type" {}
 # variable "public_zone_id" {}
