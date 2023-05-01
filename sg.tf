6c752090cf7ffc7cf393164421eab2d79a3e62be
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-05-01 11:30:47 +0530

    updating ./tf-module-mutable-app-set-up

--- sg.tf
+++ sg.tf
@@ -1,41 +1,41 @@
-#resource "aws_security_group" "main" {
-#  name        = "${var.env}-${var.name}-ec2.sg"
-#  description = "${var.env}-${var.name}-ec2.sg"
-#  vpc_id      = var.vpc_id
-#
+resource "aws_security_group" "main" {
+  name        = "${var.env}-${var.name}-ec2.sg"
+  description = "${var.env}-${var.name}-ec2.sg"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = [var.BASTION_NODE]
+  }
+
+  ingress {
+    description      = "APP"
+    from_port        = var.app_port_no
+    to_port          = var.app_port_no
+    protocol         = "tcp"
+    cidr_blocks      = [var.vpc_cidr]
+  }
+
 #  ingress {
-#    description      = "SSH"
-#    from_port        = 22
-#    to_port          = 22
+#    description      = "PROMETHEUS"
+#    from_port        = 9100
+#    to_port          = 9100
 #    protocol         = "tcp"
-#    cidr_blocks      = [var.BASTION_NODE]
+#    cidr_blocks      = [var.PROMETHEUS_NODE]
 #  }
-#
-#  ingress {
-#    description      = "APP"
-#    from_port        = var.app_port_no
-#    to_port          = var.app_port_no
-#    protocol         = "tcp"
-#    cidr_blocks      = [var.vpc_cidr]
-#  }
-#
-##  ingress {
-##    description      = "PROMETHEUS"
-##    from_port        = 9100
-##    to_port          = 9100
-##    protocol         = "tcp"
-##    cidr_blocks      = [var.PROMETHEUS_NODE]
-##  }
-#
-#  egress {
-#    from_port        = 0
-#    to_port          = 0
-#    protocol         = "-1"
-#    cidr_blocks      = ["0.0.0.0/0"]
-#    ipv6_cidr_blocks = ["::/0"]
-#  }
-#
-#  tags = {
-#    Name = "${var.env}-${var.name}-ec2.sg"
-#  }
-#}
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "${var.env}-${var.name}-ec2.sg"
+  }
+}
--- vars.tf
+++ vars.tf
@@ -5,10 +5,10 @@ variable "instance_type" {}
  variable "max_size" {}
 variable "min_size" {}
  variable "vpc_id" {}
-# variable "BASTION_NODE" {}
-# variable "app_port_no" {}
-# variable "vpc_cidr" {}
-# variable "private_zone_id" {}
+ variable "BASTION_NODE" {}
+ variable "app_port_no" {}
+ variable "vpc_cidr" {}
+ variable "private_zone_id" {}
 # variable"PROMETHEUS_NODE" {}
 #  variable "lb_listener_priority"{}
 # variable "type" {}
