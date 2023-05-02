55cd5293241e43c32268ac49c37a9de9b2ffde3f
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-05-02 10:09:09 +0530

    updating ./tf-module-mutable-app-set-up

--- asg.tf
+++ asg.tf
@@ -18,23 +18,23 @@ resource "aws_launch_template" "launch-template" {
     ENV       = var.env
   }))
 }
-#
-#resource "aws_autoscaling_group" "asg" {
-#  name               = "${var.env}-${var.name}-asg"
-#  desired_capacity   = var.min_size
-#  max_size           = var.max_size
-#  min_size           = var.min_size
-#  vpc_zone_identifier = var.subnets
-##  target_group_arns = [aws_lb_target_group.main.arn]
-#
-#  launch_template {
-#    id      = aws_launch_template.launch-template.id
-#    version = "$Latest"
-#  }
-#  tag  {
-#    key         = "Name"
-#    value       = "${var.env}-${var.name}"
-#    propagate_at_launch = true
-#  }
-#}
+
+resource "aws_autoscaling_group" "asg" {
+  name               = "${var.env}-${var.name}-asg"
+  desired_capacity   = var.min_size
+  max_size           = var.max_size
+  min_size           = var.min_size
+  vpc_zone_identifier = var.subnets
+#  target_group_arns = [aws_lb_target_group.main.arn]
+
+  launch_template {
+    id      = aws_launch_template.launch-template.id
+    version = "$Latest"
+  }
+  tag  {
+    key         = "Name"
+    value       = "${var.env}-${var.name}"
+    propagate_at_launch = true
+  }
+}
 
