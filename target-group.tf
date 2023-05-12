cc385e58c5cfdda7ca13903e4cd4f90eae5bcb78
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-05-12 16:29:43 +0530

    updating ./tf-module-mutable-app-set-up

--- target-group.tf
+++ target-group.tf
@@ -14,13 +14,13 @@ resource "aws_lb_target_group" "main" {
 }
 
 resource "aws_lb_listener_rule" "rule" {
-#  count        = var.type == "backend" ? 1:0
+  count        = var.type == "backend" ? 1:0
   listener_arn = var.alb ["private"].lb_listener_arn
-  priority     = 100
+  priority     = var.lb_listener_priority
 
   action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.static.arn
+    target_group_arn = aws_lb_target_group.main.arn
   }
 
 
