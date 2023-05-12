68b3af741e807f85f590bcc4184316cae608074b
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-05-12 15:37:37 +0530

    updating ./tf-module-mutable-app-set-up

--- target-group.tf
+++ target-group.tf
@@ -1,18 +1,18 @@
-#resource "aws_lb_target_group" "main" {
-#  name     = "${var.env}-${var.name}-tg"
-#  port     = var.app_port_no
-#  protocol = "HTTP"
-#  vpc_id   = var.vpc_id
-#  health_check {
-#    enabled = true
-#    healthy_threshold = 2
-#    unhealthy_threshold = 2
-#    timeout = 5
-#    interval =  6
-#    path = "/health"
-#  }
-#}
-#
+resource "aws_lb_target_group" "main" {
+  name     = "${var.env}-${var.name}-tg"
+  port     = var.app_port_no
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+  health_check {
+    enabled = true
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    timeout = 5
+    interval = 6
+    path = "/health"
+  }
+}
+
 #resource "aws_lb_listener_rule" "rule-frontend" {
 #  count        = var.type == "backend" ? 1:0
 #  listener_arn = var.alb ["private"].lb_listener_arn
