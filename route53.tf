27d87ad05b4346580b58b2143757a2f011ec4c1f
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-05-12 17:06:43 +0530

    updating ./tf-module-mutable-app-set-up

--- route53.tf
+++ route53.tf
@@ -1,5 +1,5 @@
 resource "aws_route53_record" "private" {
-# count   = var.type == "backend" ? 1: 0
+ count   = var.type == "backend" ? 1: 0
  zone_id = var.private_zone_id
  name    = "${var.name}-${var.env}"
  type    = "CNAME"
@@ -7,11 +7,11 @@ resource "aws_route53_record" "private" {
  records = [var.alb ["private"].lb_dns_name]
 }
 
-#resource "aws_route53_record" "public" {
-# count   = var.type == "frontend" ? 1: 0
-# zone_id = var.public_zone_id
-# name    = var.public_dns_name
-# type    = "CNAME"
-# ttl    = 300
-# records = [var.alb ["private"].lb_dns_name]
-#}
+resource "aws_route53_record" "public" {
+ count   = var.type == "frontend" ? 1: 0
+ zone_id = var.public_zone_id
+ name    = "${var.name}-${var.env}"
+ type    = "CNAME"
+ ttl     = 300
+ records = [var.alb ["public"].lb_dns_name]
+}
