733b473b6195f558d50f78c12900655adbb1bfbd
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-05-12 15:59:50 +0530

    updating ./tf-module-mutable-app-set-up

--- vars.tf
+++ vars.tf
@@ -10,8 +10,9 @@ variable "min_size" {}
  variable "vpc_cidr" {}
  variable "private_zone_id" {}
  variable"PROMETHEUS_NODE" {}
+ variable "alb" {}
 #  variable "lb_listener_priority"{}
 # variable "type" {}
-# variable "alb" {}
+
 # variable "public_zone_id" {}
 # variable "public_dns_name" {}
